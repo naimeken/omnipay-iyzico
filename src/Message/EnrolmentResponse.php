6204935ccfea2fd56c3366c4a3d1e4deaf9f3590
Tolga Can Günel <tolga@ctrlf5.com.tr>
2023-10-20 22:46:26 +0300

    taksit uyumluluğu hatası

--- src/Message/EnrolmentResponse.php
+++ src/Message/EnrolmentResponse.php
@@ -10,35 +10,40 @@ use Omnipay\Iyzico\Models\EnrolmentResponseModel;
 
 class EnrolmentResponse extends RemoteAbstractResponse implements RedirectResponseInterface
 {
-    public function __construct(RequestInterface $request, $data)
-    {
-        parent::__construct($request, $data);
+	public function __construct(RequestInterface $request, $data)
+	{
+		parent::__construct($request, $data);
 
-        $this->response = new EnrolmentResponseModel((array)$this->response);
-    }
+		$this->response = new EnrolmentResponseModel((array)$this->response);
+	}
 
-    public function getData(): EnrolmentResponseModel
-    {
-        return $this->response;
-    }
+	public function getData(): EnrolmentResponseModel
+	{
+		return $this->response;
+	}
 
-    public function isSuccessful(): bool
-    {
-        return false;
-    }
+	public function isSuccessful(): bool
+	{
+		return false;
+	}
 
-    public function isRedirect(): bool
-    {
-        return true;
-    }
+	public function getMessage(): string
+	{
+		return $this->response->errorMessage;
+	}
 
-    public function getRedirectResponse()
-    {
-        $response = parent::getRedirectResponse();
+	public function isRedirect(): bool
+	{
+		return $this->response->status === 'failure' ? false : true;
+	}
 
-        $response->setContent(str_replace("<body", "<body style='color:#FFF'", $this->getData()->threeDSHtmlContent));
+	public function getRedirectResponse()
+	{
+		$response = parent::getRedirectResponse();
 
-        $script = '<script>
+		$response->setContent(str_replace('<body', "<body style='color:#FFF'", $this->getData()->threeDSHtmlContent));
+
+		$script = '<script>
 			document.forms[0].style.display = "none";
 	        document.getElementsByTagName("section")[0].style.display = "block";
 
@@ -49,16 +54,16 @@ class EnrolmentResponse extends RemoteAbstractResponse implements RedirectRespon
 			}, 5000);
 		</script>';
 
-        $response->setContent(str_replace("</body>", "$script</body>", $response->getContent()));
+		$response->setContent(str_replace('</body>', "$script</body>", $response->getContent()));
 
-        $response->setContent(str_replace("</body>", $this->redirectSpinner() . "</body>", $response->getContent()));
+		$response->setContent(str_replace('</body>', $this->redirectSpinner() . '</body>', $response->getContent()));
 
-        return $response;
-    }
+		return $response;
+	}
 
-    protected function redirectSpinner(): string
-    {
-        $css = '<style>
+	protected function redirectSpinner(): string
+	{
+		$css = '<style>
 					section {
 					  width: 174px;
 					  margin: 0 auto;
@@ -106,7 +111,7 @@ class EnrolmentResponse extends RemoteAbstractResponse implements RedirectRespon
 					}
 </style>';
 
-        $html = '<section>
+		$html = '<section>
 		  <svg class="spinner" width="174px" height="174px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
 		     <circle class="path" fill="transparent" stroke-width="2" cx="33" cy="33" r="30" stroke="url(#gradient)"/>
 		       <linearGradient id="gradient">
@@ -122,11 +127,11 @@ class EnrolmentResponse extends RemoteAbstractResponse implements RedirectRespon
 		  </svg>
 		</section>';
 
-        return $css . $html;
-    }
+		return $css . $html;
+	}
 
-    public function getRedirectUrl()
-    {
-        return 'IYZICO-Sends-Premade-Form-So-No-Url-Needed';
-    }
+	public function getRedirectUrl()
+	{
+		return 'IYZICO-Sends-Premade-Form-So-No-Url-Needed';
+	}
 }
